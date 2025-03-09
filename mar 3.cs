912da4a0455b3f2f89f6a83d3002a6c64b19a3b4
albertoAA14 <alberto451@stemcollegiate.ca>
2025-03-09 12:51:14 -0600

    Update mar 3.cs

--- mar 3.cs
+++ mar 3.cs
@@ -1,1 +1,7 @@
+Today, I learned about literal data types in C# and how to use them in Console.WriteLine(). These data types help define the kind of data I’m working with, which prevents errors and improves clarity in my code.
 
+  Console.WriteLine('b');
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(true);
+Console.WriteLine(false);
