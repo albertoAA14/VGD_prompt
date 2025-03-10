3984d814a62d9a300fec7b5ed8257f18f13737dd
albertoAA14 <alberto451@stemcollegiate.ca>
2025-03-10 14:02:02 -0600

    Create mar 10.cs

---
+++ mar 10.cs
@@ -0,0 +1,15 @@
+mod.4 Unit 1 and 2
+
+Console.WriteLine("Hello \"World\"!");
+Console.WriteLine("Hello\tWorld!");
+Console.WriteLine("c:\\source\\repos");
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.WriteLine(@"    c:\source\repos    
+        (this is where your code goes)");
+        Console.Write(@"c:\invoices");
+
+
+     I learned how to Use character escape sequences when needed to put a special character into a literal string,
