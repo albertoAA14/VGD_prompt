acbd64ac4065e7fb8613af4850b644557b3b2094
albertoAA14 <alberto451@stemcollegiate.ca>
2025-04-07 13:49:20 -0600

    Create apr 7.cs

---
+++ apr 7.cs
@@ -0,0 +1,20 @@
+//Lesson 5 unit 1 and 2
+
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+int first = 7;
+int second = 5;
+decimal quotient = (decimal)first / (decimal)second;
+Console.WriteLine(quotient);
+
+
+I learned how to Use operators like +, -,/ to perform  mathematical operation
