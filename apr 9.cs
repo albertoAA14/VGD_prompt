d2792ed962c9769653ed571a3c7900604fc190e3
albertoAA14 <alberto451@stemcollegiate.ca>
2025-05-05 13:48:14 -0600

    Create apr 9.cs

---
+++ apr 9.cs
@@ -0,0 +1,21 @@
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
+decimal decimalQuotient = 7.0m / 5;
+Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+int value = 0;     // value is now 0.
+value = value + 5; // value is now 5.
+value += 5;        // value is now 10.
+
+
+I learned how to do basic math operations in C# like adding, subtracting, multiplying, dividing, and finding the remainder (modulus).
+
+I also learned how to update a variable’s value using += as a shortcut instead of writing value = value + something.
